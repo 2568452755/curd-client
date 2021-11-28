8d266477be324fcfffefaa997525939ef3e386ea
tygorge <2568452755@qq.com>
2021-11-28 14:46:43 +0800

    fix:rebase的提交

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,10 @@
   git的第六次提交:将develop的提交到dev中
   git的第七次提交:使用margin合并本地分支后提交
   git的第八次提交:使用rebase合并本地分支后提交
-
+  git的第九次提交:使用rebase合并本地分支后提交
+    git的第10次提交:需要使用rebase合并本地分支提交1
+    git的第11次提交:需要使用rebase合并本地分支提交2
+    git的第12次提交:需要使用rebase合并本地分支提交3
     <user />
   </div>
 </template>
